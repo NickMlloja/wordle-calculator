340a90970c5c750be63a9bdfbdf2c5fcc55ac3d0
NickMlloja <71607068+NickMlloja@users.noreply.github.com>
2026-02-13 00:56:34 -0500

    Solver Utils

--- src/frontend/solver.ts
+++ src/frontend/solver.ts
@@ -1,4 +1,9 @@
 
+export enum TileState {
+    Absent  = 0,
+    Present = 1,
+    Correct = 2,
+}
 export type WordleInput = [
     TileState,
     TileState,
@@ -25,18 +30,59 @@ export type SolutionSeries = [
     string,
 ];
 
+// #AUDIT: If a letter appears once in the solution but you guess more than once, does only one tile turn yellow?
+function guessToInput(guess: string, solution: string): WordleInput {
+
+    if (guess.length !== 5 || solution.length !== 5) {
+        throw new Error("Expected 5 letter words");
+    }
+
+    const result: WordleInput = [
+        TileState.Absent,
+        TileState.Absent,
+        TileState.Absent,
+        TileState.Absent,
+        TileState.Absent,
+    ]
+
+    // For Each Idx
+    for (let i = 0; i < 5; i++) {
+        
+        const currentChar = guess[i];
+        if(currentChar === undefined){
+            throw new Error(`Expected a character, got ${currentChar}`);
+        }
+
+        if(currentChar === solution[i]){
+            result[i] = TileState.Correct;
+        } else if (solution.includes(currentChar)) {
+            result[i] = TileState.Present;
+        } else {
+            result[i] = TileState.Absent;
+        }
+    }
+
+    return result;
 }
 
-export function getAllSolutionSeries(): Set<solutionSeries> {
-    const solutions = new Set<solutionSeries>;
+function doesGuessMatchInput(
+    guess: WordleInput,
+    solution: WordleInput
+): boolean {
+    for (let i = 0; i < 5; i++) {
+        if (guess[i] !== solution[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// #TODO: Different complexities of solver algos with measured operations
+export function getAllSolutionSeries(wordleSolution: string, wordleGraph: InputSeries): Set<SolutionSeries> {
+    const solutions = new Set<SolutionSeries>();
 
     // #TODO
 
     return solutions;
 }
 
-export enum TileState {
-    Absent  = 0,
-    Present = 1,
-    Correct = 2,
-}
